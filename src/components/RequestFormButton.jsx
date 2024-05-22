022db7f3faecf2924f08ccd3598097e3c6793444
Andi Weber <andiweber@andis-air.lan>
2024-05-22 16:45:07 -0400

    added used context to pass styling properties to buttons in different components

---
+++ src/components/RequestFormButton.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import StylesContext from "./StylesContext";
+
+const buttonStyling = React.useContext(StylesContext)
+
+export default function RequestFormButton() {
+  return (
+    <div>
+      <button>
+        Click Here!
+      </button>
+      <input type="submit" value="Submit" />
+
+    </div>
+  );
+}
